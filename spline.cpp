49f981a2f17c2bd1007854ae4081ea3ae427b267
Alec Vinson <alec.m.vinson@gmail.com>
2019-09-04 14:49:14 -0700

    add function to spline library which can find best location to loop over on orbital data

--- spline.cpp
+++ spline.cpp
@@ -1,9 +1,8 @@
 #include "spline.h"
-using namespace std;
 
-void spline(vector<double> &time, vector<double> &data, vector<double> &b, vector<double> &c, vector<double> &d)
+void spline(std::vector<double> &time, std::vector<double> &data, std::vector<double> &b, std::vector<double> &c, std::vector<double> &d)
 {
-    vector<double> h, alpha, l, mu, z;
+    std::vector<double> h, alpha, l, mu, z;
     for (int i = 0; i < time.size()-1; i++)
     {
         h.push_back(time[i+1]-time[i]);
@@ -46,3 +45,77 @@ double spline_inter_deriv(double b, double c, double d, double t, double ti)
 {
     return b + 2*c*(t-ti) + 3*d*(t-ti)*(t-ti);
 }
+
+int sim_loop(std::string file_name)
+{
+    std::ifstream input(file_name.c_str());
+    std::string last_line, line;
+    getline(input,line);
+    int ctr = 0;
+    double time, n, ecc, peri;
+    double n_prev_last, e_prev_last, peri_prev_last, time_prev_last,h;
+    input >> time;
+    do
+    {
+        ctr++;
+        h = abs(time_prev_last - time);
+        time_prev_last = time;
+        n_prev_last = n;
+        e_prev_last = ecc;
+        peri_prev_last = peri;
+        input >> n >> ecc >> peri;
+    }while(input >> time);
+    input.close();
+
+    size_t sz;
+    double time_last = time;
+    double n_last = n;
+    double e_last = ecc;
+    double peri_last = peri;
+    double d_n_dt_last = (n_last - n_prev_last)/h;
+    double d_e_dt_last = (e_last - e_prev_last)/h;
+    double d_peri_dt_last = (peri_last - peri_prev_last)/h;
+
+    input.open(file_name.c_str());
+    getline(input,line);
+    double n_prev, e_prev, peri_prev, time_prev;
+    double score = 1000.;
+    double line_best = 0;
+
+    for (int i = 1; i < ctr/2; i++)
+    {
+
+        n_prev = n;
+        e_prev = ecc;
+        peri_prev = peri;
+        input >> time >> n >> ecc >> peri;
+        if(i > 0)
+        {
+            double d_n_dt = (n - n_prev)/h;
+            double d_e_dt = (ecc - e_prev)/h;
+            double d_peri_dt = (peri - peri_prev)/h;
+            double score_test = std::pow(std::abs((d_n_dt-d_n_dt_last)/d_n_dt_last),2) + std::pow(std::abs((d_e_dt-d_e_dt_last)/d_e_dt_last),2) + std::pow(std::abs((d_peri_dt - d_peri_dt_last)/d_peri_dt_last),2)+
+            std::pow(std::abs((n_last-n)/n_last),2) + std::pow(std::abs((ecc-e_last)/e_last),2) + std::pow(std::abs((peri-peri_last)/peri_last),2);
+            //if(i % 5000 == 0)
+            if (score_test < score)
+            {
+                score = score_test;
+                line_best = i;
+            }
+
+        }
+    }
+    input.close();
+    return line_best;
+
+
+    /*psuedo:
+    Find last line in output file.
+    Look at n, n-dot, (peri, peri-dot, ecc, ecc-dot??)
+    Loop through first quarter or so of the file.
+    Find line which matches best with the values of the last line.
+        idea: find min(n/n_end + n-dot/n-dot_end + (... other variables))
+    return line number.
+    */
+}
+
--- spline.h
+++ spline.h
@@ -3,10 +3,13 @@
 
 #include <vector>
 #include <cmath>
+#include <string>
+#include <fstream>
 
 void spline(std::vector<double> &time, std::vector<double> &data, std::vector<double> &b, std::vector<double> &c, std::vector<double> &d);
 double spline_inter(double a, double b, double c, double d, double t, double ti);
 double spline_inter_deriv(double b, double c, double d, double t, double ti);
 
+int sim_loop(std::string file_name);
 
 #endif // SPLINE_H_INCLUDED
