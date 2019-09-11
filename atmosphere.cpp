13501ef36065e4b9f7ff270b62795cde39fd64e6
Alec Vinson <alec.m.vinson@gmail.com>
2019-09-11 15:43:34 -0700

    minor changes in atmosphere implementation

--- atmosphere.cpp
+++ atmosphere.cpp
@@ -10,12 +10,11 @@ Atmosphere::Atmosphere(Planet *planet)
 {
     _planet = planet;
     _mean_density = planet->get_mass() / (4.0 / 3.0 * PI * std::pow(planet->get_radius(), 3));
+    K_a = 3 * planet->get_stellar_mass() * std::pow(planet->get_radius(), 3) / (5.0 * _mean_density * std::pow(planet->get_semi_major(), 3));
 }
 
 double Atmosphere::damp(Planet *planet)
 {
-    double K_a = 3 * planet->get_stellar_mass() * std::pow(planet->get_radius(), 3) / (5.0 * _mean_density * std::pow(planet->get_semi_major(), 3)); //still needs to be initialized
-
     double sigma = 2.0 * planet->get_gamma_dot();
 
     return -3.0/2.0 * 3.0 * K_a * b_a(sigma);
--- atmosphere.h
+++ atmosphere.h
@@ -21,6 +21,8 @@ private:
     //Following taken from Table 1, Leconte et al. 2015
     double _q_0 = (365. * 24. * 3600.) * (365. * 24. * 3600.) * 1000.0; //amplitude of the atmospheric quadrupole. pressure, SI except time in years.
     double _omega_0 = 71.0; //intrinsic thermal frequency of the atmosphere
+    double K_a;
+
     double b_a(double freq);
 };
 
